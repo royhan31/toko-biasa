1e2cf9ae04b696c6055633ced28aa1b08b44efcc
hisyammaulana <hisyammaulana100@gmail.com>
2019-02-04 22:06:25 +0700

    perbarui tabel

---
+++ database/migrations/2019_02_04_144031_create_customer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fullname');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password');
+            $table->string('telephone', 13);
+            $table->text('address');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
